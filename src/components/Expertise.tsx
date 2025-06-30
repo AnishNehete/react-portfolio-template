7e1ce3129e0769104c002a76ac3b5eb68080dd2d
Anish Nehete <anishnehete@hotmail.com>
2025-06-29 18:45:51 -0700

    Fixing Issues

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import {faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 import { faCloud, faDatabase, faFaceGrinWink, faGraduationCap, faChartLine } from "@fortawesome/free-solid-svg-icons";
